2252cd95b482632265640713be853fb57e626f3d
mydeveloperplanet <gunter@mydeveloperplanet.com>
2025-02-23 15:17:26 +0100

    Added controller with compile errors

---
+++ src/main/java/com/mydeveloperplanet/myaicodeprojectplanet/controller/CustomersController.java
@@ -0,0 +1,65 @@
+package com.mydeveloperplanet.myaicodeprojectplanet.controller;
+
+import com.mydeveloperplanet.myaicodeprojectplanet.model.Customer;
+import com.mydeveloperplanet.myaicodeprojectplanet.openapi.api.CustomersApi;
+import com.mydeveloperplanet.myaicodeprojectplanet.openapi.model.Customer;
+import com.mydeveloperplanet.myaicodeprojectplanet.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomersController implements CustomersApi {
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Override
+    public ResponseEntity<List<com.mydeveloperplanet.myaicodeprojectplanet.openapi.model.Customer>> customersGet() {
+        List<Customer> customers = customerService.getAllCustomers();
+        return new ResponseEntity<>(customers, HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> customersPost(@RequestBody com.mydeveloperplanet.myaicodeprojectplanet.openapi.model.Customer openAPICustomer) {
+        Customer customer = convertToDomainModel(openAPICustomer);
+        Customer createdCustomer = customerService.createCustomer(customer);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<com.mydeveloperplanet.myaicodeprojectplanet.openapi.model.Customer> customersIdGet(@PathVariable Long id) {
+        Optional<Customer> customerOptional = customerService.getCustomerById(id);
+        if (customerOptional.isPresent()) {
+            return new ResponseEntity<>(customerOptional.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @Override
+    public ResponseEntity<Void> customersIdPut(@PathVariable Long id, @RequestBody com.mydeveloperplanet.myaicodeprojectplanet.openapi.model.Customer openAPICustomer) {
+        Customer customerDetails = convertToDomainModel(openAPICustomer);
+        Customer updatedCustomer = customerService.updateCustomer(id, customerDetails);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @Override
+    public ResponseEntity<Void> customersIdDelete(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    private Customer convertToDomainModel(com.mydeveloperplanet.myaicodeprojectplanet.openapi.model.Customer openAPICustomer) {
+        return new Customer(
+                openAPICustomer.getId(),
+                openAPICustomer.getFirstName(),
+                openAPICustomer.getLastName()
+        );
+    }
+}
