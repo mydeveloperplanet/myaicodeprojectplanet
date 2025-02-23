97a24f4a5d708a9eeea564ace8f6176fdc113372
mydeveloperplanet <gunter@mydeveloperplanet.com>
2025-02-23 12:11:10 +0100

    Initial commit

---
+++ src/main/java/com/mydeveloperplanet/myaicodeprojectplanet/MyAiCodeProjectPlanetApplication.java
@@ -0,0 +1,13 @@
+package com.mydeveloperplanet.myaicodeprojectplanet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAiCodeProjectPlanetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAiCodeProjectPlanetApplication.class, args);
+	}
+
+}
